471e5f52a8794162e571608f754e7117f8707a5b
Eoin Coll <ecoll@sfu.ca>
2018-07-09 17:16:22 -0700

    testing with wav player

--- mp3towav.c
+++ mp3towav.c
@@ -10,23 +10,25 @@
 #include <stdlib.h>
 // #include <alsa/asoundlib.h>
 #include <string.h>
+#include <stdbool.h>
 //#include "lame/lame.h"
+#include "playwav.c"
 
 #define INFILE "audiofiles/test1.mp3"
 #define OUTFILE "audiofiles/out.wav"
 #define MAX_LENGTH 1000
 
+_Bool running = true;
+_Bool playing = false;
+
 int main(int argc, char* argv[])
 {
-  printf("testing\n");
-
   char command[MAX_LENGTH];
 
   // strcpy(command, "touch audiofiles/output.wav");
   strcpy(command, "lame --decode audiofiles/test1.mp3 audiofiles/output.wav");
   system(command);
 
-  return 0;
 
   //
   // FILE *mp3 = fopen(INFILE, "rf");
@@ -41,5 +43,14 @@ int main(int argc, char* argv[])
   //
   // fclose(mp3);
   // fclose(wav);
+  while (running)
+  {
+    printf("running\n");
+    if(playing)
+    {
+      printf("playing\n");
+    }
+  }
 
+  return 0;
 }
