91882ee257bd7eb7dfe9c32b773d3d03ef2afc58
Chris <cla235@sfu.ca>
2018-07-25 22:10:30 -0700

    interface changes, mostly to accommodate control module

--- control.h
+++ control.h
@@ -41,13 +41,7 @@ void control_cleanup(void);
 /**
  * Set the music player to be in master mode
  */
-void control_setMasterMode(void);
-
-/**
- * Set the music player to be in slave mode
- * @param sa Address of master device
- */
-void control_setSlaveMode(struct sockaddr_in sa);
+void control_setMode(enum control_mode m);
 
 /**
  * Gets the current mode set for the BBG
@@ -71,6 +65,12 @@ void control_playAudio(void);
  * Pause audio
  */
 void control_pauseAudio(void);
+
+/**
+ * Get whether the device is playing audio
+ * @return Play status
+ */
+int control_getPlayStatus(void);
 
 /**
  * Skip the current song being played
@@ -111,18 +111,6 @@ const song_t *control_getQueue(void);
  * @return Address to the next song_t object (immutable)
  */
 const song_t *control_getNextSong(void);
-
-/**
- * Set the volume for audio playback
- * @param vol Volume level from 0 to 100
- */
-void control_setVolume(unsigned int vol);
-
-/**
- * Get the current volume level of the device
- * @return Volume level from 0 to 100
- */
-unsigned int control_getVolume(void);
 
 /**
  * Register a slave device
